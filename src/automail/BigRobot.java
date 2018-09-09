db51c2fb3dccc7050608fb4e9aa93be9651db15f
threethousandmonkeys <nicole.cheung7171@gmail.com>
2018-09-09 17:38:28 +1000

    RIGHT VERSION KINDA

--- src/automail/BigRobot.java
+++ src/automail/BigRobot.java
@@ -12,12 +12,10 @@ public class BigRobot extends Robot{
      *
      * @param delivery governs the final delivery
      * @param mailPool is the source of mail items
-     * @param strong   is whether the robot can carry heavy items
      */
     public BigRobot(IMailDelivery delivery, IMailPool mailPool) {
         super(delivery, mailPool, true);
         setMaxItems(6);
         getTube().setTubeSize(getMaxItems());
     }
-
 }
