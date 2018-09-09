0dd3edab15e08c4fe208cb13efa093ac24c4b52d
threethousandmonkeys <nicole.cheung7171@gmail.com>
2018-09-09 15:19:08 +1000

    RIGHT VERSION KINDA

--- src/automail/RobotBehaviour.java
+++ src/automail/RobotBehaviour.java
@@ -1,13 +1,15 @@
 package automail;
 
 import exceptions.ExcessiveDeliveryException;
+import exceptions.FragileItemBrokenException;
+import exceptions.ItemTooHeavyException;
 
 public interface RobotBehaviour {
     // TODO should this be an interface or an abstract class?
     // This is trying to use the adaptor pattern
     void dispatch();
-    void step() throws ExcessiveDeliveryException;
-    void setRoute();
-    void moveTowards(int destination);
+    void step() throws ExcessiveDeliveryException, ItemTooHeavyException, FragileItemBrokenException;
+    void setRoute() throws ItemTooHeavyException;
+    void moveTowards(int destination) throws FragileItemBrokenException;
 
 }
