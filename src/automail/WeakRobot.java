1731dd2886a78048f8e192dcb291659afd7987b6
Zhuoping Miao <zhuopingm@github.com>
2018-09-09 15:21:17 +1000

    I don't know

---
+++ src/automail/WeakRobot.java
@@ -0,0 +1,18 @@
+package automail;
+
+import strategies.IMailPool;
+
+public class WeakRobot extends Robot {
+
+    /**
+     * Initiates the robot's location at the start to be at the mailroom
+     * also set it to be waiting for mail.
+     *
+     * @param delivery governs the final delivery
+     * @param mailPool is the source of mail items
+     * @param strong   is whether the robot can carry heavy items
+     */
+    public WeakRobot(IMailDelivery delivery, IMailPool mailPool, boolean strong) {
+        super(delivery, mailPool, strong);
+    }
+}
