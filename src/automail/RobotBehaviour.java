f13674b46884366a7495753b9f42223596f6c4f1
nofun97 <novan.allanadi1211@gmail.com>
2018-09-11 15:32:30 +1000

    Removed TODO

--- src/automail/RobotBehaviour.java
+++ src/automail/RobotBehaviour.java
@@ -6,8 +6,6 @@ import exceptions.ItemTooHeavyException;
 import exceptions.NoValidRobotsAvailableException;
 
 public interface RobotBehaviour {
-    // TODO should this be an interface or an abstract class?
-    // This is trying to use the adaptor pattern
     void dispatch();
     void step() throws ExcessiveDeliveryException, ItemTooHeavyException, FragileItemBrokenException, NoValidRobotsAvailableException;
     void setRoute() throws ItemTooHeavyException;
